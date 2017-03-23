c158bebf8c103149c1db6fd39f6a7d2f3cfb0065
Dustin Tran <dustinviettran@gmail.com>
2017-03-23 13:17:10 -0400

    replace greek args with english args in tests/

--- tests/test-inferences/test_integer_variables.py
+++ tests/test-inferences/test_integer_variables.py
@@ -26,22 +26,22 @@ class test_integer_init(tf.test.TestCase):
     N, D = X_train.shape
     n_hidden = 10
     K = 10
-    W_0 = Normal(mu=tf.zeros([D, n_hidden]), sigma=tf.ones([D, n_hidden]))
-    W_1 = Normal(mu=tf.zeros([n_hidden, K]), sigma=tf.ones([n_hidden, K]))
-    b_0 = Normal(mu=tf.zeros(n_hidden), sigma=tf.ones(n_hidden))
-    b_1 = Normal(mu=tf.zeros(K), sigma=tf.ones(K))
+    W_0 = Normal(loc=tf.zeros([D, n_hidden]), scale=tf.ones([D, n_hidden]))
+    W_1 = Normal(loc=tf.zeros([n_hidden, K]), scale=tf.ones([n_hidden, K]))
+    b_0 = Normal(loc=tf.zeros(n_hidden), scale=tf.ones(n_hidden))
+    b_1 = Normal(loc=tf.zeros(K), scale=tf.ones(K))
 
     y = Categorical(logits=neural_network(X_train, W_0, W_1, b_0, b_1))
 
     softplus = tf.nn.softplus
-    qW_0 = Normal(mu=tf.Variable(tf.random_normal([D, n_hidden])),
-                  sigma=softplus(tf.Variable(tf.random_normal([D, n_hidden]))))
-    qW_1 = Normal(mu=tf.Variable(tf.random_normal([n_hidden, K])),
-                  sigma=softplus(tf.Variable(tf.random_normal([n_hidden, K]))))
-    qb_0 = Normal(mu=tf.Variable(tf.random_normal([n_hidden])),
-                  sigma=softplus(tf.Variable(tf.random_normal([n_hidden]))))
-    qb_1 = Normal(mu=tf.Variable(tf.random_normal([K])),
-                  sigma=softplus(tf.Variable(tf.random_normal([K]))))
+    qW_0 = Normal(loc=tf.Variable(tf.random_normal([D, n_hidden])),
+                  scale=softplus(tf.Variable(tf.random_normal([D, n_hidden]))))
+    qW_1 = Normal(loc=tf.Variable(tf.random_normal([n_hidden, K])),
+                  scale=softplus(tf.Variable(tf.random_normal([n_hidden, K]))))
+    qb_0 = Normal(loc=tf.Variable(tf.random_normal([n_hidden])),
+                  scale=softplus(tf.Variable(tf.random_normal([n_hidden]))))
+    qb_1 = Normal(loc=tf.Variable(tf.random_normal([K])),
+                  scale=softplus(tf.Variable(tf.random_normal([K]))))
     inference = ed.KLqp({W_0: qW_0, b_0: qb_0, W_1: qW_1, b_1: qb_1},
                         data={y: y_train})
     inference.run()
--- tests/test-inferences/test_scale.py
+++ tests/test-inferences/test_scale.py
@@ -14,10 +14,10 @@ class test_inference_scale_class(tf.test.TestCase):
   def test_scale_0d(self):
     N = 10
     M = 5
-    mu = Normal(mu=0.0, sigma=1.0)
-    x = Normal(mu=tf.ones(M) * mu, sigma=tf.ones(M))
+    mu = Normal(loc=0.0, scale=1.0)
+    x = Normal(loc=tf.ones(M) * mu, scale=tf.ones(M))
 
-    qmu = Normal(mu=tf.Variable(0.0), sigma=tf.constant(1.0))
+    qmu = Normal(loc=tf.Variable(0.0), scale=tf.constant(1.0))
 
     x_ph = tf.placeholder(tf.float32, [M])
     data = {x: x_ph}
@@ -28,10 +28,10 @@ class test_inference_scale_class(tf.test.TestCase):
   def test_scale_1d(self):
     N = 10
     M = 5
-    mu = Normal(mu=0.0, sigma=1.0)
-    x = Normal(mu=tf.ones(M) * mu, sigma=tf.ones(M))
+    mu = Normal(loc=0.0, scale=1.0)
+    x = Normal(loc=tf.ones(M) * mu, scale=tf.ones(M))
 
-    qmu = Normal(mu=tf.Variable(0.0), sigma=tf.constant(1.0))
+    qmu = Normal(loc=tf.Variable(0.0), scale=tf.constant(1.0))
 
     x_ph = tf.placeholder(tf.float32, [M])
     data = {x: x_ph}
